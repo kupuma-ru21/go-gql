88dbc1bce0a623fe3883b6a88907e3e2103bda52
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-09 23:27:58 +0900

    feat: add complexity limit

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/playground"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/volatiletech/sqlboiler/v4/boil"
@@ -35,10 +36,19 @@ func main() {
 
 	service := services.New(db)
 
-	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{
-		Srv:     service,
-		Loaders: graph.NewLoaders(service),
-	}}))
+	srv := handler.NewDefaultServer(
+		internal.NewExecutableSchema(
+			internal.Config{
+				Resolvers: &graph.Resolver{
+					Srv:     service,
+					Loaders: graph.NewLoaders(service),
+				},
+				Complexity: graph.ComplexityConfig(),
+			},
+		),
+	)
+
+	srv.Use(extension.FixedComplexityLimit(10))
 
 	boil.DebugMode = true
 
